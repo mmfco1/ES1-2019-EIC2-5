399710b687d3f119e16e3ff2b8d76f76245ef2b5
fsvaa-iscteiul <fsvaa@iscte-iul.pt>
2019-12-10 14:48:15 +0000

    test

--- ProjetoES2/src/FileReader.java
+++ ProjetoES2/src/FileReader.java
@@ -1,38 +1,41 @@
-//import java.io.File;
-//import java.io.IOException;
-//
-//import org.apache.poi.EncryptedDocumentException;
-//import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
-//import org.apache.poi.ss.usermodel.Cell;
-//import org.apache.poi.ss.usermodel.DataFormatter;
-//import org.apache.poi.ss.usermodel.Row;
-//import org.apache.poi.ss.usermodel.Sheet;
-//import org.apache.poi.xssf.usermodel.XSSFSheet;
-//import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-//
+import java.io.File;
+import java.io.IOException;
+import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 public class FileReader {
 
-//	private XSSFWorkbook wb;
-//	private XSSFSheet sheet;
+	private XSSFWorkbook wb;
+	private XSSFSheet sheet;
 
 	public FileReader() {
 
 	};
+	
+	/*
+	 * importaçao do exel
+	 * Afonso
+	 */
 
+	public String[][] createCols(File file) throws InvalidFormatException, IOException {
+		String[][] cols = new String[7][12];
+		wb = new XSSFWorkbook(file);
+		sheet = wb.getSheetAt(0);
+		DataFormatter dataFormatter = new DataFormatter();
 
-//	public String[][] createCols(File file) throws InvalidFormatException, IOException {
-//		String[][] cols = new String[7][12];
-//		wb = new XSSFWorkbook(file);
-//		sheet = wb.getSheetAt(0);
-//		DataFormatter dataFormatter = new DataFormatter();
-//
-//		for (Row row : sheet) {
-//			for (Cell cell : row) {
-//				String cellValue = dataFormatter.formatCellValue(cell);
-//				cols[cell.getRowIndex()][cell.getColumnIndex()] = cellValue;
-//			}
-//		}
-//		return cols;
-//	}
+		for (Row row : sheet) {
+			for (Cell cell : row) {
+				String cellValue = dataFormatter.formatCellValue(cell);
+				cols[cell.getRowIndex()][cell.getColumnIndex()] = cellValue;
+			}
+		}
+		return cols;
+	}
 
 }
--- ProjetoES2/src/Gui.java
+++ ProjetoES2/src/Gui.java
@@ -10,23 +10,27 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
-import javax.swing.JTextArea;
 import javax.swing.ScrollPaneConstants;
 import javax.swing.WindowConstants;
 import javax.swing.table.DefaultTableModel;
 
-//import org.apache.poi.EncryptedDocumentException;
-//import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 
 public class Gui implements ActionListener {
 
 	private JFrame frame;
 	private JTable table;
-	private JButton choose;
+	private JButton choose, thresholds;
 	private String[][] cols;
 	private final String[] ROWS = {"MethodID", "Package", "Class", "Method", "LOC", "CYCLO", "ATFD",
 			"LAA", "is_long_method", "iPlasma", "PMD", "is_feature_envy"};
-
+	
+	
+	/*
+	 * criacao do gui
+	 * Francisco Veiga
+	 */
 	private void gui() {
 		frame = new JFrame();
 		JPanel mainP = new JPanel();
@@ -37,6 +41,7 @@ public class Gui implements ActionListener {
 		table.setModel(dtm);
 		JScrollPane scrollP = new JScrollPane(table);
 		choose = new JButton("Choose");
+		thresholds = new JButton("Tresholds");
 
 
 		mainP.setLayout(new BorderLayout());
@@ -47,11 +52,13 @@ public class Gui implements ActionListener {
 
 		mainP.add(middleP, BorderLayout.CENTER);
 		mainP.add(bottomP, BorderLayout.SOUTH);
-		bottomP.add(choose, BorderLayout.CENTER);
-		bottomP.add(choose);
+		bottomP.add(choose, BorderLayout.EAST);
+		bottomP.add(thresholds, BorderLayout.WEST);
+
 		middleP.add(scrollP, BorderLayout.CENTER);
 
 		choose.addActionListener(this);
+		thresholds.addActionListener(this);
 
 		frame.setSize(1500, 600);
 		frame.setContentPane(mainP);
@@ -63,10 +70,11 @@ public class Gui implements ActionListener {
 	public Gui() {
 		gui();
 	}
-	/*
-	 * DESCOMENTAR TUDO O POSSIVEL NA GUI E NO FILEREADER
-	 */
 
+	/*
+	 * acoes dos buttoes
+	 * Francisco, Afonso
+	 */
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		try {
@@ -84,11 +92,11 @@ public class Gui implements ActionListener {
 				fileC.showOpenDialog(frame);
 				FileReader fr = new FileReader();
 				File file = fileC.getSelectedFile();
-//				try {
-//					String[][] temp = fr.createCols(file);
-//					DefaultTableModel dtm = new DefaultTableModel(temp, ROWS);
-//					table.setModel(dtm);
-//				} catch (EncryptedDocumentException | InvalidFormatException | IOException e1) {e1.printStackTrace();}
+				try {
+					String[][] temp = fr.createCols(file);
+					DefaultTableModel dtm = new DefaultTableModel(temp, ROWS);
+					table.setModel(dtm);
+				} catch (EncryptedDocumentException | InvalidFormatException | IOException e1) {e1.printStackTrace();}
 				
 			}
 
